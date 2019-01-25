fecfc01f4f25f6e96e78345cbd1e0d638c7b0d13
Vishu Chaudhary <jaatvishalsanserwal@gmail.com>
2019-01-25 11:25:27 +0530

    Update LandingUI.js

--- src/components/presentational/LandingUI.js
+++ src/components/presentational/LandingUI.js
@@ -3,11 +3,10 @@ import React from 'react';
 const LandingUI = (props)=>{
     return <div className="landing-container container">
             <div className="row">
-                <div className="col-sm-12 col-md-6 ">
-                     <p>Test</p>
+                <div className="col-12 text-center">
+                     <p>Edit LandingUI.js</p>
                 </div>
                 <div className="col-sm-12 col-md-6 ">
-                     <p>Test</p>
                 </div>
 
             </div>
